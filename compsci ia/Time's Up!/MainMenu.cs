34edc1d4e9c58f7f9f364a0d7f24bfd1bd979e33
BuildTools <unconfigured@null.spigotmc.org>
2022-09-07 13:38:18 +0200

    Project backup for updating C# version to 8.0

---
+++ compsci ia/Time's Up!/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time_s_Up_
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var taskForm = new Task_Tester.taskpage();
+            taskForm.Show();
+
+            this.Hide();
+
+        }
+    }
+}
